ed39f19491ed55cf3be5b2ec5ebbfcc037d4c591
HendrylXamann <estudos123@gmail.com>
2024-04-26 10:50:18 -0300

    iniciando projeto

---
+++ env-local.sh
@@ -0,0 +1,4 @@
+export STATIC_FOLDER='/media/hendryl/HD/Programacao/linked-list---project/static'
+export TEMPLATE_FOLDER='/media/hendryl/HD/Programacao/linked-list---project/templates'
+export ICON_PATH=''
+export API_URL='http://127.0.0.1:5001'
---
+++ models/model.py
@@ -0,0 +1,7 @@
+
+class Criar_usuario():
+    def __init__(self,nome,veiculo,cnh):
+        self.nome = nome
+        self.veiculo = veiculo
+        self.cnh = cnh
+    
