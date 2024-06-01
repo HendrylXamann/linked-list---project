1d1ae65cfabf5c1cf2940c3ba1603d588935434e
Hendryl Macedo <hendryl.macedo@gochange.com.br>
2024-05-31 22:25:36 -0300

    70% complete

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from flask import Flask, render_template, request
 import os
 import logging
-from models.model import *
+from models.modeltp1 import *
+from models.modeltp2 import *
 
 path = os.path.dirname(os.path.abspath(__file__))
 static_folder = os.path.join(path, 'static')
@@ -20,9 +21,11 @@ usu = Lista_Usuarios()
 usuarios = [("Kant", "moto", "sim"), ("Nietzsche", "moto", "sim"), ("Schopenhauer", "moto", "sim"), ("Hegel", "moto", "sim")]
 [usu.adicionar_usuario(*usuario) for usuario in usuarios]
 
-rotasList = ListaRotas()
 rotas = [(1, 3, "Kant"), (2, 2, "Nietzsche"), (3, 1, "Schopenhauer"), (4, 3, "Hegel")]
-[rotasList.adicionar_rota(Rotas(id=id, prioridade=prioridade, funcionario=funcionario)) for id, prioridade, funcionario in rotas]
+arvore_rotas = ArvoreBiBusca()
+[arvore_rotas.inserir(id, funcionario, prioridade) for id, prioridade, funcionario in rotas]
+rotasList = arvore_rotas.listar_rotas()
+
 
 @app.route("/")
 def home():
@@ -32,18 +35,8 @@ def home():
 def manual():
     return render_template("user_manual.html")
 
-@app.route("/create_user", methods=["GET", "POST"])
-def create_user():
-    if request.method == "POST":
-        nome = request.form["nome"]
-        veiculo = request.form["tipo_veiculo"]
-        cnh = request.form["cnh"]
-        usu.adicionar_usuario(nome, veiculo, cnh)
-        return render_template("manage_users.html", message=f'Usuario {nome} criado com sucesso!')
-    return render_template("manage_users.html")
-
-@app.route("/routes", methods=["GET", "POST"])
-def route_management():
+@app.route("/routesTP1", methods=["GET", "POST"])
+def route_managementTp1():
     usuarios = usu.listar_usuarios()
     tdas_rotas = rotasList.listar_rotas()
     if request.method == "POST":
@@ -68,6 +61,41 @@ def route_management():
         return render_template("route_management.html", rotas=tdas_rotas, funcionarios=usuarios, message=f'Alteração realizada!')
 
     return render_template("route_management.html", rotas=tdas_rotas, funcionarios=usuarios)
+
+@app.route("/create_route", methods=["GET", "POST"])
+def create_route():
+    new_rotas = ArvoreBiBusca()
+    if request.method == "POST":
+        n_rota = request.form["rota_id"]
+        funcionario = request.form["funcionario_rota"]
+        prioridade = request.form["prioridade_rota"]
+        new_rotas.inserir(n_rota, funcionario,prioridade)
+        return render_template("create_route.html", message='Rota criada com sucesso!')
+    return render_template("create_route.html")
+
+@app.route("/routes", methods=["GET", "POST"])
+def route_managementTp2():
+    usuarios = usu.listar_usuarios()
+    tdas_rotas = arvore_rotas.listar_rotas()
+    if request.method == "POST":
+        for rota in tdas_rotas:
+            rota_id = rota[0]  # rota é uma tupla (id, funcionario, prioridade)
+            novo_funcionario = request.form["funcionario_" + str(rota_id)]
+            nova_prioridade = int(request.form["prioridade_" + str(rota_id)])
+            arvore_rotas.alterar_prioridade(rota_id, nova_prioridade)
+            
+            novo_usuario = next((usuario for usuario in usuarios if usuario.nome == novo_funcionario), None)
+            if novo_usuario:
+                arvore_rotas.alterar_funcionario(rota_id, novo_usuario)
+
+            if request.form.get("excluir_" + str(rota_id)):
+                arvore_rotas.delete(rota_id)
+
+        tdas_rotas = arvore_rotas.listar_rotas()
+        return render_template("route_management.html", rotas=tdas_rotas, funcionarios=usuarios, message='Alteração realizada!')
+
+    return render_template("route_management.html", rotas=tdas_rotas, funcionarios=usuarios)
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5001, debug=True)
---
+++ models/modeltp2.py
@@ -0,0 +1,110 @@
+class Noh:
+    def __init__(self, rota, funcionario, prioridade):
+        self.rota = rota
+        self.funcionario = funcionario
+        self.prioridade = prioridade
+        self.esquerda = None
+        self.direita = None
+
+class ArvoreBiBusca:
+    def __init__(self):
+        self.raiz = None
+
+    def inserir(self, rota, funcionario, prioridade):
+        novo_noh = Noh(rota, funcionario, prioridade)
+        if self.raiz is None:
+            self.raiz = novo_noh
+        else:
+            self._inserir(self.raiz, novo_noh)
+
+    def _inserir(self, noh, novo_noh):
+        if novo_noh.prioridade < noh.prioridade:
+            if noh.esquerda is None:
+                noh.esquerda = novo_noh
+            else:
+                self._inserir(noh.esquerda, novo_noh)
+        else:
+            if noh.direita is None:
+                noh.direita = novo_noh
+            else:
+                self._inserir(noh.direita, novo_noh)
+
+    def delete(self, rota):
+        self.raiz = self._delete(self.raiz, rota)
+
+    def _delete(self, noh, rota):
+        if noh is None:
+            return noh
+
+        if rota < noh.rota:
+            noh.esquerda = self._delete(noh.esquerda, rota)
+        elif rota > noh.rota:
+            noh.direita = self._delete(noh.direita, rota)
+        else:
+            if noh.esquerda is None:
+                return noh.direita
+            elif noh.direita is None:
+                return noh.esquerda
+
+            temp = self._menorValorNoh(noh.direita)
+            noh.rota = temp.rota
+            noh.funcionario = temp.funcionario
+            noh.prioridade = temp.prioridade
+            noh.direita = self._delete(noh.direita, temp.rota)
+        return noh
+
+    def _menorValorNoh(self, noh):
+        current = noh
+        while(current.esquerda is not None):
+            current = current.esquerda
+        return current
+
+    def busca(self, rota):
+        return self._busca(self.raiz, rota)
+
+    def _busca(self, noh, rota):
+        if noh is None or noh.rota == rota:
+            return noh
+        if noh.rota < rota:
+            return self._busca(noh.direita, rota)
+        return self._busca(noh.esquerda, rota)
+    
+    def listar_rotas(self):
+        return self._listar_rotas(self.raiz, [])
+
+    def _listar_rotas(self, noh, rotas):
+        if noh:
+            self._listar_rotas(noh.esquerda, rotas)
+            rotas.append((noh.rota, noh.funcionario, noh.prioridade))
+            self._listar_rotas(noh.direita, rotas)
+        return rotas
+    
+    def inserir_por_prioridade(self, rota, funcionario, prioridade):
+        novo_noh = Noh(rota, funcionario, prioridade)
+        if self.raiz is None:
+            self.raiz = novo_noh
+        else:
+            self._inserir_por_prioridade(self.raiz, novo_noh)
+
+    def _inserir_por_prioridade(self, noh, novo_noh):
+        if novo_noh.prioridade < noh.prioridade:
+            if noh.esquerda is None:
+                noh.esquerda = novo_noh
+            else:
+                self._inserir_por_prioridade(noh.esquerda, novo_noh)
+        else:
+            if noh.direita is None:
+                noh.direita = novo_noh
+            else:
+                self._inserir_por_prioridade(noh.direita, novo_noh)
+    
+    def alterar_prioridade(self, rota_id, nova_prioridade):
+        noh = self.busca(rota_id)
+        if noh:
+            self.delete(rota_id)
+            self.inserir_por_prioridade(noh.rota, noh.funcionario, nova_prioridade)
+
+    def alterar_funcionario(self, rota_id, novo_funcionario):
+        noh = self.busca(rota_id)
+        if noh:
+            noh.funcionario = novo_funcionario
